71b37a1c74ee9061df57db90b39aaa267b7138dc
Kacper Hernacki <hernackikacper@gmail.com>
2025-01-15 16:53:29 +0100

    feat: external api

--- external/app.ts
+++ external/app.ts
@@ -1,8 +1,13 @@
-import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
-import { OpenAIService } from './OpenAIService';
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
+import { OpenAIService } from "./OpenAIService";
 import type { ChatCompletion } from "openai/resources/chat/completions";
-import { chatLimiter, authMiddleware, errorHandler, validationMiddleware } from './middlewares';
+import {
+  chatLimiter,
+  authMiddleware,
+  errorHandler,
+  validationMiddleware,
+} from "./middlewares";
 
 const app = express();
 const port = 3000;
@@ -13,20 +18,35 @@ app.use(express.json());
 app.use(errorHandler);
 
 // Server initialization
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
+
+// app.post("/api/chat", (req, res) => {
+//   console.log("lol");
+//   res.json({ lol: "lol" });
+// });
 
 // Routes
-app.post('/api/chat', chatLimiter, authMiddleware, validationMiddleware, async (req, res, next) => {
-  const { messages, conversation_id = uuidv4() } = req.body;
+app.post(
+  "/api/chat",
+  chatLimiter,
+  authMiddleware,
+  validationMiddleware,
+  async (req, res, next) => {
+    const { messages, conversation_id = uuidv4() } = req.body;
 
-  try {
-    const answer = await openaiService.completion({ messages }) as ChatCompletion;
-    res.json({ ...answer, conversation_id });
-  } catch (error) {
-    next(error);
+    try {
+      const answer = (await openaiService.completion({
+        messages,
+      })) as ChatCompletion;
+      res.json({ ...answer, conversation_id });
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
